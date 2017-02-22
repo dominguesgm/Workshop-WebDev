3d4e5a892708844c9bdb3fce5d6abaae81f197df
Gil Domingues <gmdz95@gmail.com>
2017-02-22 00:17:41 +0000

    added population

--- code/database/database.sql
+++ code/database/database.sql
@@ -30,3 +30,6 @@ INSERT INTO cadeiras (sigla, nome, ano, semestre, curso) VALUES ('SOPE', 'Sistem
 INSERT INTO cadeiras (sigla, nome, ano, semestre, curso) VALUES ('TSIN', 'Teoria do Sinal', '2', '1', 'MIEEC');
 INSERT INTO apontamentos (cadeira, nome, descricao, autor) VALUES (1, 'Conceitos de Assembly', 'Apontamentos tirados durante a introducao a assebly', 'Daniel Silva');
 INSERT INTO apontamentos (cadeira, nome, descricao, autor) VALUES (3, 'Threads', 'Apontamentos sobre threads', 'Gil Domingues');
+INSERT INTO apontamentos (cadeira, nome, descricao, autor) VALUES (2, 'Portas Logicas', 'Apontamentos sobre portas logicas', 'Miguel Ramalho');
+INSERT INTO apontamentos (cadeira, nome, descricao, autor) VALUES (4, 'Sinais', 'Sinal da cruz', 'Filipa Barros');
+INSERT INTO apontamentos (cadeira, nome, descricao, autor) VALUES (3, 'Semaphores', 'Magnificos apontamentos de semaforos e sinais de stop', 'Rui Vilares');
